47083021783544a632178204086fd9362ba4bdd7
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-08 20:15:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  function handleClick (){
+    alert('Button Clicked');
+  }
+  return (
+    <>
+      <h3>React Core Concepts 2</h3>
+      <button>Click Me</button>
+    </>
+  )
+}
+
+export default App
