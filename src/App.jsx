09542bc2c5ba32c10374b4cd11c91f830a94623c
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-08 20:39:55 +0600

    explored onClick events

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import './App.css'
 
 function App() {
 
-  function handleClick (){
+  function handleClick() {
     alert('Button Clicked');
   }
+
+  const handleClick2 = () => {
+    alert('Button 2 Clicked')
+  }
+
+  const addFive = (num) => {
+    alert(num + 5);
+  }
+
   return (
     <>
       <h3>React Core Concepts 2</h3>
-      <button>Click Me</button>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick2}>Button 2</button>
+      <button onClick={() => { alert('3rd Button Clicked') }}>Button 3</button>
+      <button onClick={() => addFive(3)}>Button 4</button>
     </>
   )
 }
